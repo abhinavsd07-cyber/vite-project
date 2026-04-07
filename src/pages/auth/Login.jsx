6f0e280416a4be90b90db6e64a19d4df4f97226e
Abhinav S D <abhinavsd07@gmail.com>
2026-04-07 09:37:39 +0530

    test ok

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -6,8 +6,8 @@ import { AuthLayout } from "../../components/auth/AuthLayout";
 import { loginAPI } from "../../services/allApis";
 
 export const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("superadmin@gmail.com");
+  const [password, setPassword] = useState("Admin@123");
   const [showPassword, setShowPassword] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
